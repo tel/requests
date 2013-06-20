afac63db3d645907c55635fbc17b50a81c44b634
Joseph Abrahamson <me@jspha.com>
2013-06-19 20:17:14 -0400

    Adjusted a few of the parsers.

--- src/URI.hs
+++ src/URI.hs
@@ -4,7 +4,6 @@
 module URI where
 
 import Text.ParserCombinators.UU
-import qualified Text.ParserCombinators.UU.Utils as U
 import Text.ParserCombinators.UU.BasicInstances
 import Text.ParserCombinators.UU.Idioms
 
@@ -61,33 +60,57 @@ data Either3 a b c   = Ei1 a | Ei2 b | Ei3 c deriving (Eq, Show, Read, Ord)
 -- $small-parses
 
 newtype Query        = Query        String
+                       deriving (Show, Eq, Ord)
 newtype Fragment     = Fragment     String
+                       deriving (Show, Eq, Ord)
 newtype Param        = Param        String
+                       deriving (Show, Eq, Ord)
 data    Segment      = Segment      String [Param]
+                       deriving (Show, Eq, Ord)
 newtype PathSegments = PathSegments (NonEmpty Segment)
+                       deriving (Show, Eq, Ord)
 newtype Port         = Port         Int
+                       deriving (Show, Eq, Ord)
 newtype IPv4Address  = IPv4Address  (Int, Int, Int, Int)
+                       deriving (Show, Eq, Ord)
 data    HostName     = HostName
                        [String]       -- ^ Zero or more Domain Names
                        String         -- ^ A TLD
                        Bool           -- ^ Is it an FQDN? It ended in '.'?
+                       deriving (Show, Eq, Ord)
 newtype Host         = Host         (Either HostName IPv4Address)
+                       deriving (Show, Eq, Ord)
 data    HostPort     = HostPort     Host (Maybe Port)
+                       deriving (Show, Eq, Ord)
 newtype UserInfo     = UserInfo     String
+                       deriving (Show, Eq, Ord)
 data    Server       = Server       (Maybe UserInfo) HostPort
+                       deriving (Show, Eq, Ord)
 newtype RegName      = RegName      String
+                       deriving (Show, Eq, Ord)
 newtype Authority    = Authority    (Either Server RegName)
+                       deriving (Show, Eq, Ord)
 newtype Scheme       = Scheme       String
+                       deriving (Show, Eq, Ord)
 newtype RelSegment   = RelSegment   String
+                       deriving (Show, Eq, Ord)
 newtype AbsPath      = AbsPath      PathSegments
+                       deriving (Show, Eq, Ord)
 data    NetPath      = NetPath      (Maybe Authority) (Maybe AbsPath)
+                       deriving (Show, Eq, Ord)
 data    RelPath      = RelPath      RelSegment (Maybe AbsPath)
+                       deriving (Show, Eq, Ord)
 newtype OpaquePart   = OpaquePart   String
+                       deriving (Show, Eq, Ord)
 data    HierPart     = HierPart     (Either NetPath AbsPath) (Maybe Query)
+                       deriving (Show, Eq, Ord)
 newtype Path         = Path         (Either AbsPath OpaquePart)
+                       deriving (Show, Eq, Ord)
 data    RelativeURI  = RelativeURI  (Either3 NetPath AbsPath RelPath)
                                     (Maybe Query)
+                       deriving (Show, Eq, Ord)
 data    AbsoluteURI  = AbsoluteURI  Scheme (Either HierPart OpaquePart)
+                       deriving (Show, Eq, Ord)
 
 pFragment     :: Parser Fragment
 pFragment     = Fragment    <$> pMany pUriC  <?> "fragment"
@@ -141,7 +164,7 @@ pHost         :: Parser Host
 pHost         = Host        <$> pEither pHostName pIPv4Address
 
 pHostPort     :: Parser HostPort
-pHostPort     = HostPort    <$> pHost <*> (pMaybe pPort)
+pHostPort     = HostPort    <$> pHost <*> pMaybe (pSym ':' *> pPort)
 
 pUserInfo     :: Parser UserInfo
 pUserInfo     = UserInfo    <$> pMany (    pUnreserved
@@ -149,7 +172,7 @@ pUserInfo     = UserInfo    <$> pMany (    pUnreserved
                                        <|> pAnyOf ";:&=+$," )
 
 pServer       :: Parser Server
-pServer       = Server      <$> (pMaybe pUserInfo <* pSym '@')
+pServer       = Server      <$> pMaybe (pUserInfo <* pSym '@')
                             <*> pHostPort
 
 pRegName      :: Parser RegName
@@ -175,9 +198,9 @@ pAbsPath      :: Parser AbsPath
 pAbsPath      = AbsPath     <$> (pSym '/' *> pPathSegments)
 
 pNetPath      :: Parser NetPath
-pNetPath      = pString "//"
-                *> ( NetPath <$> pMaybe pAuthority
-                             <*> pMaybe pAbsPath   )
+pNetPath      = NetPath     <$  pString "//"
+                            <*> pMaybe pAuthority
+                            <*> pMaybe pAbsPath
 
 pRelPath      :: Parser RelPath
 pRelPath      = RelPath     <$> pRelSegment
@@ -192,7 +215,7 @@ pOpaquePart   = go          <$> (    pUnreserved
 
 pHierPart     :: Parser HierPart
 pHierPart     = HierPart    <$> pEither pNetPath pAbsPath
-                            <*> (pSym '?' *> pMaybe pQuery)
+                            <*> pMaybe (pSym '?' *> pQuery)
 
 pPath         :: Parser Path
 pPath         = Path        <$> pEither pAbsPath pOpaquePart
@@ -201,7 +224,7 @@ pRelativeURI  :: Parser RelativeURI
 pRelativeURI  = RelativeURI <$> (    (Ei1 <$> pNetPath)
                                  <|> (Ei2 <$> pAbsPath)
                                  <|> (Ei3 <$> pRelPath) )
-                            <*> (pSym '?' *> pMaybe pQuery)
+                            <*> pMaybe (pSym '?' *> pQuery)
 
 pAbsoluteURI  :: Parser AbsoluteURI
 pAbsoluteURI  = AbsoluteURI <$> ( pScheme <* pSym ':' )
